105076d7b1e506ba66ff02549fd1808edf59df28
Razshal <fontenille.mathieu@gmail.com>
2019-05-23 17:17:37 +0200

    ex02 - pause menu working

---
+++ d03/Assets/Scripts/pauseScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class pauseScript : MonoBehaviour {
+    private gameManager gameManager;
+    public GameObject pauseCanvas;
+
+    public void Resume() {
+        pauseCanvas.SetActive(false);
+        gameManager.pause(false);
+    }
+
+	void Start () {
+        gameManager = GameObject.Find("GameManager").GetComponent<gameManager>();
+	}
+	
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape) && !pauseCanvas.activeSelf)
+        {
+            pauseCanvas.SetActive(true);
+            gameManager.pause(true);
+        }
+	}
+}
