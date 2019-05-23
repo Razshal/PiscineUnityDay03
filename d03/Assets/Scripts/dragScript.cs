6554afbc164f59ced44355701d1895ec49b6581f
Razshal <fontenille.mathieu@gmail.com>
2019-05-23 13:31:10 +0200

    ex01 - wip

---
+++ d03/Assets/Scripts/dragScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class dragScript : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+    public static GameObject dragged;
+    private Vector3 originPos;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        dragged = gameObject;
+        originPos = gameObject.transform.position;
+    }
+
+	public void OnDrag(PointerEventData eventData)
+    {
+        gameObject.transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        dragged = null;
+        gameObject.transform.position = originPos;
+    }
+}
