97e0d57392a59daefa807871d093e59757ed3e55
Razshal <fontenille.mathieu@gmail.com>
2019-05-23 12:27:15 +0200

    ex00 - done

---
+++ d03/Assets/Scripts/loadSceneScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadSceneScript : MonoBehaviour {
+	public void LoadSceneOnClick()
+	{
+        SceneManager.LoadScene("ex01");
+	}
+    public void QuitSceneOnClick()
+    {
+        Application.Quit();
+    }
+}
